e869a14c69c196efa656c13c420f2096f6b26cff
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-08-15 00:23:47 +0530

    Added solution - LeetHub

---
+++ Flip Bits - GFG/flip-bits.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.maxOnes(a, n));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+
+    public static int maxOnes(int a[], int n) {
+        // Your code goes here
+         int current=0;
+       int ones=0;
+       int max=0;
+       for(int i=0;i<n;i++){
+           if(a[i]==1){
+               ones++;
+           }
+           
+           int val=0;
+           val=(a[i]==1)?-1:1;
+           
+           current=Math.max(current+val,val);
+           max=Math.max(max,current);
+         
+           
+       }
+       
+       return max+ones;
+    }
+}
